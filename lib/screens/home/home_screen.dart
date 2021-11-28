4ea770e4966263a3d0a93d2e938114dc9c9b94cb
Samuel Owadayo <odevservices@gmail.com>
2021-11-28 07:25:49 +0100

    first commit

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_ui/components/bottom_nav_bar.dart';
+import 'package:ecommerce_ui/enums.dart';
+
+import 'components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = "/home";
+
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
+    );
+  }
+}
---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_ui/components/bottom_nav_bar.dart';
+import 'package:ecommerce_ui/enums.dart';
+
+import 'components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static String routeName = "/profile";
+
+  const ProfileScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: Body(),
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.profile),
+    );
+  }
+}
